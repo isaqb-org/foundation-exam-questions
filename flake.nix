2b1f9c261bf44b3a74e641eb7f11570fd80d30a0
Mike Sperber <sperber@deinprogramm.de>
2025-02-11 14:18:13 +0100

    Fix description in Nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "iSAQB Foundation Exam Code";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+      devShells = {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.gradle
+            pkgs.racket
+            pkgs.libxml2
+          ];
+        };
+      };
+    });
+}

106a1afd230e43de62d5d96fa445653cb82463d7
Mike Sperber <sperber@deinprogramm.de>
2025-10-21 11:45:46 +0200

    Add jing-trang to Nix dev shell

--- flake.nix
+++ flake.nix
@@ -37,6 +37,7 @@
               self.packages.${system}.make-exam
               self.packages.${system}.tag-release
               pkgs.mustache-go
+              pkgs.jing-trang # for schema conversion
             ];
           };
         };
